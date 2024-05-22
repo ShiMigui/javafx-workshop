29e5631708fde50872485d1e27dc4c2802aff57a
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-05-22 17:43:41 -0300

    :sparkles: fea: change Alerts and Confirmations's constructors to private

--- src/gui/alerts/Alerts.java
+++ src/gui/alerts/Alerts.java
@@ -3,6 +3,9 @@ package gui.alerts;
 import javafx.scene.control.Alert.AlertType;
 
 public class Alerts extends AlertController {
+    private Alerts() {
+    }
+
     public static void showAlert(AlertType type, String title, String header, String content) {
 	Alerts.setContent(type, title, header, content);
 	alert.show();
--- src/gui/alerts/Confirmations.java
+++ src/gui/alerts/Confirmations.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
 
-public class Confirmations extends AlertController {
+public final class Confirmations extends AlertController {
+    private Confirmations() {
+    }
+    
     public static Optional<ButtonType> showAlert(String title, String header, String content) {
 	Confirmations.setContent(AlertType.CONFIRMATION, title, header, content);
 	return alert.showAndWait();
